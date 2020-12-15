551ee641a74701a2f722b7f5552cf4ecb5ee8496
Chigbogu Orji <brightorji60@gmail.com>
2020-12-15 13:32:03 +0100

    Modify schemas

--- applications/model.js
+++ applications/model.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose')
 const Applications = new mongoose.Schema({
 	company:    { type: mongoose.Types.ObjectId, ref: 'Companies' },
 	job:        { type: mongoose.Types.ObjectId, ref: 'Jobs' },
-	candidates: { type: mongoose.Types.ObjectId, ref: 'Candidates' },
-	is_open:    {type: Boolean, default: true}
+	candidates: { type: mongoose.Types.ObjectId, ref: 'Candidates' }
 })
 
-module.exports = mongoose.model('Applications', Applications, 'applications')
+module.exports = mongoose.model('Apps', Applications, 'apps')
--- jobs/model.js
+++ jobs/model.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
 const Jobs = new mongoose.Schema({
-	tags:                  [String],
-	is_open:               {type: Boolean, default: true},
-	title:                 [String, 'job title is required'],
-	description:           [String, 'job description is required'],
-	requisites:            [String, ' should set job requisites'],
-	skills:                [{ type: mongoose.Types.ObjectId, ref: 'Skills' }],
-	applications_received: {type: mongoose.Types.ObjectId, ref: 'Applications'},
-	company:               {type: mongoose.Types.ObjectId, ref: 'Company'}
+	tags:         [String],
+	is_open:      {type: Boolean, default: true},
+	title:        [String, 'job title is required'],
+	description:  [String, 'job description is required'],
+	requisites:   [String, ' should set job requisites'],
+	skills:       [{ type: mongoose.Types.ObjectId, ref: 'Skills' }],
+	applications: [{type: mongoose.Types.ObjectId, ref: 'Apps'}],
+	company:      {type: mongoose.Types.ObjectId, ref: 'Company'}
 })
+
+module.exports = mongoose.model('Jobs', Jobs, 'jobs')
