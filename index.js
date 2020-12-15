1dd59131a3950f547a9d1beb55af193e13135dcb
Chigbogu Orji <brightorji60@gmail.com>
2020-12-15 10:53:43 +0100

    Add post routes for error handling

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 require('dotenv').config()
-const config = require('./config')(process.env.NODE_ENV) // can initialize with an arg
+const config = require('./config')(process.env.NODE_ENV)
 const app = require('express')()
 const mongoose = require('mongoose')
 
 require('./middlewares/pre-route')(app)
 app.use('/ping', (req, res) => res.sendStatus(200))
 app.use('/api/v1', require('./routes'))
+require('./middlewares/post-route')(app)
 
-app.listen(config.port, () => {
-	require('./db.config')(mongoose, config)
-	console.log('server listening on port ::', config.port)
-})
+if (require.main === module) {
+	app.listen(config.port, () => {
+		require('./db.config')(mongoose, config)
+		console.log('server listening on port ::', config.port)
+	})
+} else module.exports = app
--- middlewares/post-route.js
+++ middlewares/post-route.js
@@ -1,8 +1,19 @@
 const response = require('../util/response')
 module.exports = (app) => {
-  app.use((err, req, res, next) => {
-    if (err.name === 'CustomError') {
-      return res.status(err.status).json(response(404, err.message, null, false))
-    }
-  })
+	const possibleErrorsNames = ['CastError', 'JsonWebTokenError', 'ValidationError', 'SyntaxError', 'MongooseError', 'MongoError']
+
+	app.use((req, res, next) => next())
+
+	app.use((err, req, res, next) => { // eslint-disable-line
+		if (err.name == 'CustomError') {
+			return res.status(error.status).json(response(err.status, err.message, null, false))
+		} else if (err.name == 'MongoError' && err.code == 11000) {
+			return res.status(400).json(response(400, 'Validation failed, fields already exist', null, false))
+		} else if (possibleErrorsNames.indexOf(err.name) > -1) {
+			return res.status(400).json(response(400, err.message, null, false))
+		} else {
+			console.log('5xx Error >>> ', err)
+			res.status(500).send(response(500, err.message, null, false))
+		}
+	})
 }
